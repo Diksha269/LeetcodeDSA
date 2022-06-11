156bb3ded108fde31759ff4ecfb22e1690c70ff9
Diksha Naithani <95649667+Diksha269@users.noreply.github.com>
2022-06-11 11:50:23 +0530

    Time: 1 ms (54.40%), Space: 41.8 MB (66.25%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -1,32 +1,14 @@
 class Solution {
-    public TreeNode bstFromPreorder(int[] preorder) {
-        return solve(preorder , 0 , preorder.length-1 );
+int i = 0;
+    public TreeNode bstFromPreorder(int[] arr) {
+        return helper(arr, Integer.MAX_VALUE);
     }
-    public TreeNode solve( int[] nums , int left , int right){
-// if right subtree do not exist ..that means only left subtree is there
-// so pass left to right as there is no mid
-        
-// if left subtree do not exist ...means mid is there left will become greater than right as right is passed as mid - 1 and we will return null
-    
-// if left and right is same means only single element is there ..no left or right subtree exists
 
-// mid refers to start of right subtree
-        
-        if(left > right ) return null;
-        TreeNode root = new TreeNode(nums[left]);
-        if(left == right) return root;
-        int mid = right + 1;
-        int max = nums[left];
-        for(int i = left + 1 ; i <= right ; ++i){
-            if(nums[i] > max){
-                mid = i;
-                break;
-            }
-        }
-        root.left = solve(nums , left + 1 , mid-1);
-        root.right = solve(nums , mid , right);
+    public TreeNode helper(int[] arr, int bound) {
+        if (i == arr.length || arr[i] > bound) return null;
+        TreeNode root = new TreeNode(arr[i++]);
+        root.left = helper(arr, root.val);
+        root.right = helper(arr, bound);
         return root;
     }
-}
-        
-        
+	}
