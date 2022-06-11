82510d44def92b8242353453bd483cc1ba66a0e0
Diksha Naithani <95649667+Diksha269@users.noreply.github.com>
2022-06-11 11:38:07 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (96.19%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -3,6 +3,14 @@ class Solution {
         return solve(preorder , 0 , preorder.length-1 );
     }
     public TreeNode solve( int[] nums , int left , int right){
+// if right subtree do not exist ..that means only left subtree is there
+// so pass left to right as there is no mid
+        
+// if left subtree do not exist ...means mid is there left will become greater than right as right is passed as mid - 1 and we will return null
+    
+// if left and right is same means only single element is there ..no left or right subtree exists
+
+// mid refers to start of right subtree
         
         if(left > right ) return null;
         TreeNode root = new TreeNode(nums[left]);
