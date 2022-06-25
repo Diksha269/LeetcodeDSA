5531478efbe6980b0cbd5523998d04a431fbaff7
Diksha Naithani <95649667+Diksha269@users.noreply.github.com>
2022-06-25 13:04:37 +0530

    Time: 4 ms (83.99%), Space: 45.7 MB (27.90%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -9,22 +9,18 @@ class Solution {
     
     public void solve(int [] nums , int target , List<Integer> ans , int idx){
       if(target == 0){
+          // storing in result only when target has become zero
          result.add(new ArrayList<Integer>(ans));
           return;
       }
-        if(idx == nums.length){
-            return;}
-        if(target < 0) {
-            
-            return;}
-      
-          ans.add(nums[idx]);
-          solve(nums, target-nums[idx] ,ans,idx );
-          ans.remove(ans.size()-1);
+        if(idx == nums.length || target < 0) return;
+        
           solve(nums, target,ans,idx+1 );
-        
-    
-        
+          if(target>= nums[idx]){
+          ans.add(nums[idx]);
+          solve(nums, target-nums[idx] ,ans,idx);
+          ans.remove(ans.size()-1); // backtrack 
+          }
     
    }
 }
