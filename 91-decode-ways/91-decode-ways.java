1cee6cb3cc9a44e57cc1c109b42bfe358604c612
Diksha Naithani <95649667+Diksha269@users.noreply.github.com>
2022-07-05 10:37:26 +0530

    Time: 3 ms (41.04%), Space: 42.3 MB (53.68%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,38 +1,24 @@
-class Solution {
-    int [] dp ;
+public class Solution {
     public int numDecodings(String s) {
-        dp = new int[101];
-        Arrays.fill(dp , -1);
-       char[] str = s.toCharArray();
-  
-        return decodeWays(str,0);
-    }
-    
-    public int decodeWays(char [] str , int i ){
-        if(dp[i] != -1) return dp[i];
-        if(str.length<=i){
-            return 1;
+        if (s == null || s.length() == 0) {
+            return 0;
         }
-       
-        
-        if(str[i] == '0' ) return 0;
-        
-        // alone
-        int ans1 = 0 ,ans2= 0,ans3=0;
-        
-            ans1 = decodeWays(str, i+1);
-        
-        // pair check if pair is not greater than 26
-        if(str.length-i>=2){
-       
-     if(str[i]<'2')
-           ans2 = decodeWays(str,i+2);
-     else if(str[i]=='2' && str[i+1] <='6')
-            ans3= decodeWays(str,i+2);
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+        for (int i = 2; i <= n; i++) {
+            int first = Integer.valueOf(s.substring(i - 1, i));
+            int second = Integer.valueOf(s.substring(i - 2, i));
+            //alone
+            if (first >= 1 && first <= 9) {
+               dp[i] += dp[i-1];  
+            }
+            // pair
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i-2];
+            }
         }
-        
-        
-        return dp[i] =  ans1 + ans2+ans3;
+        return dp[n];
     }
 }
-         
