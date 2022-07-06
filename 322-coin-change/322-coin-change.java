210514960a6adb1d4d0541e6b54b85e8d8ca0a1b
Diksha Naithani <95649667+Diksha269@users.noreply.github.com>
2022-07-06 12:37:40 +0530

    Time: 118 ms (10.20%), Space: 42.7 MB (84.35%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,32 +1,52 @@
 class Solution {
-    int min = Integer.MAX_VALUE-1;
-    int memo[];
+    int memo[][];
     public int coinChange(int[] coins, int amount) {
-        memo = new int[amount+1];
-        Arrays.fill(memo , -1);
-        System.out.println(min);
-        int ans = solve(coins, amount);
-        System.out.println(ans);
-        if(ans >= (int)Math.pow(10,5)) return -1;
+        memo = new int[coins.length
++1][amount+1];
+        for(int i = 0 ; i<=coins.length ; i++){
+            for(int j = 0 ; j<=amount ; j++){
+                memo[i][j]=-1;
+            }
+        }
+        int ans = solve(coins, amount, coins.length);
+        if(ans >= (int)Math.pow(10,9)) return -1;
         else return ans;
         
         }
     
-    public int solve(int[] coins, int amount ){
-    if(memo[amount] != -1) return memo[amount];
-      int ans = (int)Math.pow(10,5); 
-      if(amount == 0) return 0;
-      for(int i = 0; i<coins.length; ++i){
-          // include
-          int include = 0;
-          if(coins[i]<= amount){
-              include= 1+solve(coins , amount - coins[i]);
-              ans = Math.min(ans , include);
+//     public int solve(int[] coins, int amount ){
+//     if(memo[amount] != -1) return memo[amount];
+//       int ans = (int)Math.pow(10,5); 
+//       if(amount == 0) return 0;
+//       for(int i = 0; i<coins.length; ++i){
+//           // include
+//           int include = 0;
+//           if(coins[i]<= amount){
+//               include= 1+solve(coins , amount - coins[i]);
+//               ans = Math.min(ans , include);
               
-          }
+//           }
               
-          }
+//           }
        
-        return memo[amount]=ans;
+//         return memo[amount]=ans;
+//     }
+    
+    public int solve(int [] coins , int amount , int n ){
+      if(memo[n][amount] != -1) return memo[n][amount];
+        
+        if(amount == 0) return 0;
+        
+         if(n == 0) 
+        // if(amount%coins[0] == 0) return amount/coins[0];
+        return (int)Math.pow(10,9);
+    
+        //take 
+        int take = (int)Math.pow(10,9); 
+        if(coins[n-1]<= amount) take = 1+solve(coins , amount - coins[n-1] , n);
+        int nottake = solve(coins , amount , n-1);
+        
+        return memo[n][amount]=Math.min(take , nottake);
+        
     }
 }
