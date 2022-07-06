558ee02a8433c535917e54597e0318fdf9d6afa9
Diksha Naithani <95649667+Diksha269@users.noreply.github.com>
2022-07-06 11:43:02 +0530

    Time: 73 ms (14.55%), Space: 45.2 MB (47.95%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -7,14 +7,14 @@ class Solution {
         System.out.println(min);
         int ans = solve(coins, amount);
         System.out.println(ans);
-        if(ans >= (int)Math.pow(10,9)) return -1;
+        if(ans >= (int)Math.pow(10,5)) return -1;
         else return ans;
         
         }
     
     public int solve(int[] coins, int amount ){
     if(memo[amount] != 0) return memo[amount];
-      int ans = (int)Math.pow(10,9); 
+      int ans = (int)Math.pow(10,5); 
       if(amount == 0) return 0;
       for(int i = 0; i<coins.length; ++i){
           // include
