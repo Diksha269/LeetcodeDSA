c8343d5f79eb2e7e1c92507aaab76007be0a4e57
Diksha Naithani <95649667+Diksha269@users.noreply.github.com>
2022-07-06 11:40:58 +0530

    Time: 91 ms (11.86%), Space: 45.5 MB (35.54%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,32 @@
+class Solution {
+    int min = Integer.MAX_VALUE-1;
+    int memo[];
+    public int coinChange(int[] coins, int amount) {
+        memo = new int[amount+1];
+        Arrays.fill(memo , 0);
+        System.out.println(min);
+        int ans = solve(coins, amount);
+        System.out.println(ans);
+        if(ans >= (int)Math.pow(10,9)) return -1;
+        else return ans;
+        
+        }
+    
+    public int solve(int[] coins, int amount ){
+    if(memo[amount] != 0) return memo[amount];
+      int ans = (int)Math.pow(10,9); 
+      if(amount == 0) return 0;
+      for(int i = 0; i<coins.length; ++i){
+          // include
+          int include = 0;
+          if(coins[i]<= amount){
+              include=  1+solve(coins , amount - coins[i]);
+              ans = Math.min(ans , include);
+              
+          }
+              
+          }
+       
+        return memo[amount]=ans;
+    }
+}
