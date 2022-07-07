c4024c4248187a5baa76c5362ff196fc749ccc1d
Diksha Naithani <95649667+Diksha269@users.noreply.github.com>
2022-07-07 11:33:20 +0530

    Time: 0 ms (100.00%), Space: 42 MB (19.28%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -7,13 +7,32 @@ class Solution {
     }
     
     public int rob(int [] nums , int n){
-        if(memo[n]!= -1) return memo[n];
-        if(n==0) return 0;
-        int include = nums[n-1];
-        if(n>2){
-         include = nums[n-1]+rob(nums , n-2);
+        int[] dp = new int[n+1];
+        //dp[i] means maximum amount stolen when size of nums is i.
+        dp[0] = 0; // when nums is empty
+        for(int i = 1 ; i<= n ; i++){
+            int include = nums[i-1];
+            if(i>1) include = nums[i-1] + dp[i-2];
+            dp[i] = Math.max(dp[i-1] ,include );
+            
         }
-        int notinclude = rob(nums, n-1);
-        return memo[n]= Math.max(include ,notinclude);
+        return dp[n];
     }
 }
+    
+    
+    
+    
+    
+//     // returns the maximum amount upto n;
+//     public int rob(int [] nums , int n){
+//         if(memo[n]!= -1) return memo[n];
+//         if(n==0) return 0;
+//         int include = nums[n-1];
+//         if(n>2){
+//          include = nums[n-1]+rob(nums , n-2); // if curr idx is included that means , n-1 can't be included 
+//         }
+//         int notinclude = rob(nums, n-1);
+//         return memo[n]= Math.max(include ,notinclude);
+//     }
+// }
