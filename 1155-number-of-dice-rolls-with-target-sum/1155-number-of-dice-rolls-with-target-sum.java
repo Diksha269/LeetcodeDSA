e5d80ba9bb18d18a16e057c459401730fc8a57ae
Diksha Naithani <95649667+Diksha269@users.noreply.github.com>
2022-08-10 12:30:35 +0530

    Time: 35 ms (56.82%), Space: 42.7 MB (49.13%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,33 +1,13 @@
 class Solution {
     
-    long dp[][] ;
-    
-    public int numRollsToTarget(int n, int k, int target) {
-       dp = new long[30+1][1000+1];
-        for(int i = 0 ; i<= 30; i++){
-            for(int j = 0 ; j<= 1000 ; j++){
-                dp[i][j]=-1;
-            }
-        }
-        int ans = (int) solve(n , k , target);
-        return ans;
+   public int numRollsToTarget(int d, int f, int target) {
+        Integer[][] mem=new Integer[d+1][target+1];
+        return numRollsToTarget(d,f,target,mem);
     }
-    public long solve(int n, int k, int target){
-        
-    
-        if(dp[n][target] != -1) return dp[n][target];
-        if(n==0 && target ==0) return 1;
-        if(n==0 && target !=0) return 0;
-        
-        long ans = 0;
-        for(int i = 1 ; i<= k ; ++i){
-            
-           if(target-i>=0) ans = ans + solve(n-1 , k , target - i);
-           else break;
-        }
-        
-        return dp[n][target] = ans % 1000000007;
-    }
-    
-    
-}
+    public int numRollsToTarget(int d, int f, int target, Integer[][] mem) {
+        if(d==0||target<0) return target==0?1:0;
+        if(mem[d][target]!=null) return mem[d][target];
+        int ways=0;
+        for(int i=1;i<=f;i++) ways=(ways+numRollsToTarget(d-1,f,target-i,mem))%1000000007; 
+        return mem[d][target]=ways;
+    }}
