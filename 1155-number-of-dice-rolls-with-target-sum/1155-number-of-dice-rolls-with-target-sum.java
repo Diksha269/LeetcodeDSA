bc46a0ed923eba677c9108def705de6573d79b89
Diksha Naithani <95649667+Diksha269@users.noreply.github.com>
2022-08-10 12:47:43 +0530

    Time: 40 ms (50.35%), Space: 42.5 MB (61.56%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,13 +1,11 @@
 class Solution {
-    
-   public int numRollsToTarget(int d, int f, int target) {
-        Integer[][] mem=new Integer[d+1][target+1];
-        return numRollsToTarget(d,f,target,mem);
-    }
-    public int numRollsToTarget(int d, int f, int target, Integer[][] mem) {
-        if(d==0||target<0) return target==0?1:0;
-        if(mem[d][target]!=null) return mem[d][target];
-        int ways=0;
-        for(int i=1;i<=f;i++) ways=(ways+numRollsToTarget(d-1,f,target-i,mem))%1000000007; 
-        return mem[d][target]=ways;
+     public int numRollsToTarget(int d, int f, int target) {
+     
+        int[][] dp=new int[d+1][target+1];
+        dp[0][0]=1;
+        for(int i=1;i<=d;i++)
+            for(int j=1;j<=target;j++)
+                for(int k=1;k<=f;k++)
+                    dp[i][j]=(dp[i][j]+(k<=j?dp[i-1][j-k]:0))%1000000007;
+        return dp[d][target];
     }}
