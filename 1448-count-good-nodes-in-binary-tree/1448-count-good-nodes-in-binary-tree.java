63735369c8c888e85f53cd2d5449dc5f9218717e
Diksha Naithani <95649667+Diksha269@users.noreply.github.com>
2022-09-01 23:24:20 +0530

    Time: 2 ms (100.00%), Space: 50.3 MB (98.43%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -1,45 +1,23 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     int count = 0;
     public int goodNodes(TreeNode root) {
         if(root == null) return 0;
         ArrayList<Integer> list = new ArrayList<>();
-        // list.add(root.val);
         if(root.left == null && root.right == null) return 1;
-        solve(root , list);
+        solve(root , root.val);
         return count;
         
     }
-    public void solve(TreeNode root , ArrayList<Integer> list ){
+    public void solve(TreeNode root , int maxSoFar ){
         
         if(root == null) return ;
-        boolean flag = false;
-        for(int i = 0 ; i<list.size() ; i++){
-            if(root.val < list.get(i)) {
-                flag = true;
-                break;
-            }
+        if(maxSoFar <= root.val) {
+            maxSoFar = root.val;
+            count++;
         }
-        list.add(root.val);
-        if(flag == false) count++;
-        solve(root.left,list);
-        solve(root.right,list);
-        list.remove(list.size()-1);
         
-        
+        solve(root.left,maxSoFar);
+        solve(root.right,maxSoFar);
+         
     }
 }
