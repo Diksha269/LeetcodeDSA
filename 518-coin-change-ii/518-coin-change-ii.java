b253e1d81e8b56e714a193c05c0a76ff37991140
Diksha Naithani <95649667+Diksha269@users.noreply.github.com>
2022-09-24 21:44:46 +0530

    Time: 90 ms (8.25%), Space: 125.2 MB (5.01%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.java
+++ 518-coin-change-ii/518-coin-change-ii.java
@@ -2,7 +2,7 @@ class Solution {
     Integer dp [][];
     public int change(int amount, int[] coins) {
         dp = new Integer[301][5001];
-        return solve(coins, amount , coins.length);
+        return solveWithTabulation(coins, amount );
     }
     
     public int solve(int [] arr , int amount , int n){
@@ -14,5 +14,32 @@ class Solution {
             return dp[n][amount] = solve(arr, amount , n-1)+ solve(arr,amount - arr[n-1] , n);
         }
         else return dp[n][amount] = solve(arr, amount , n-1);
+    }
+    
+    public int solveWithTabulation(int [] arr , int amount){
+        int n = arr.length;
+        int [][] dp = new int[n+1][amount+1];
+        // dp[i][j] -> total unique ways of getting j amount with i coins
+        
+        dp[0][0] = 1;
+        for(int i = 1 ; i<= n ; i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1 ; i<= amount ; i++){
+            dp[0][i] = 0;
+        }
+        for(int i = 1 ; i<= n ; i++){
+            for(int j = 1 ; j<= amount; j++){
+                if(arr[i-1] <= j){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-arr[i-1]];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][amount];
+        
+        
     }
 }
