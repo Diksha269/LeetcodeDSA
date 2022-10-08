f191d173e593b62fe6d3ea58316e3b45f6cc009a
Diksha Naithani <95649667+Diksha269@users.noreply.github.com>
2022-10-08 18:24:26 +0530

    Time: 8 ms (12.63%), Space: 43.1 MB (9.48%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
@@ -1,33 +1,71 @@
 class Solution {
-    int[] costs;
-    Integer[] memo;
-    Set<Integer> dayset;
-
+    int [] dp;
     public int mincostTickets(int[] days, int[] costs) {
-        this.costs = costs;
-        memo = new Integer[366];
-        dayset = new HashSet();
-        for (int d: days) dayset.add(d);
-
-        return dp(1);
+        dp = new int [1000];
+        Arrays.fill(dp,-1);
+        return solve(days, costs, days[0]);
     }
+    
+    public int solve(int [] nums , int [] costs , int day){
+        if(dp[day] != -1) return dp[day];
+        
+         if( search(nums,day) == -1 && UpperBound(nums,day) == -1) return 0;
+     
+       
+         if(search(nums,day) == -1){
+            int x = UpperBound(nums,day);
+            return dp[day]=Math.min(costs[0]+solve(nums, costs , nums[x]+1),Math.min(costs[1]+solve(nums,costs,nums[x]+7),
+        costs[2]+solve(nums,costs,nums[x]+30)));
+        }
+        else return  dp[day]= Math.min(costs[0]+solve(nums, costs , day+1),Math.min(costs[1]+solve(nums,costs,day+7),
+        costs[2]+solve(nums,costs,day+30)));
+       
+    }
+    
+  
+    public  int UpperBound(int a[], int x) {// x is the key or target value
+   int low = 0 ;
+   int high = a.length;
+   int mid = 0;
+     // Till low is less than high
+     while (low < high && low != a.length) {
+     
+        mid = low + (high - low) / 2;
 
-    public int dp(int i) {
-        if (i > 365)
-            return 0;
-        if (memo[i] != null)
-            return memo[i];
-
-        int ans;
-        if (dayset.contains(i)) {
-            ans = Math.min(dp(i+1) + costs[0],
-                               dp(i+7) + costs[1]);
-            ans = Math.min(ans, dp(i+30) + costs[2]);
-        } else {
-            ans = dp(i+1);
+        if (x >= a[mid]) {
+            low = mid + 1;
         }
 
-        memo[i] = ans;
-        return ans;
+    
+        else {
+            high = mid;
+        }
     }
-}
+    if (low == a.length ) {
+      
+         return -1;      
+    }
+    else return low;
+
+    }
+   public  int search(int [] arr, int x){
+    int l = 0, r = arr.length - 1; 
+		while (l <= r) { 
+			int m = l + (r - l) / 2; 
+			if (arr[m] == x) 
+				return m; 
+
+			if (arr[m] < x) 
+				l = m + 1; 
+			else
+				r = m - 1; 
+		} 
+
+		return -1; 
+	} 
+
+ }
+ 
+    
+    
+    
