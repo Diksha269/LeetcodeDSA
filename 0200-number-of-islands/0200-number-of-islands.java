beec932fbec4a478781a20687b9e627ebe1bba09
Diksha Naithani <95649667+Diksha269@users.noreply.github.com>
2022-10-30 00:09:47 +0530

    Time: 6 ms (56.78%), Space: 57.6 MB (35.37%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -13,9 +13,9 @@ class Solution {
     }
     
     public void dfs(int i , int j , int r , int c , char[][] grid){
-        if(i<0 || i>=r || j<0 || j>=c || grid[i][j] == '0' || grid[i][j] == '2') return;
+        if(i<0 || i>=r || j<0 || j>=c || grid[i][j] == '0' ) return;
      
-        grid[i][j] = '2';
+        grid[i][j] = '0';
         dfs(i,j+1,r,c,grid);
         dfs(i+1,j,r,c,grid);
         dfs(i-1,j,r,c,grid);
